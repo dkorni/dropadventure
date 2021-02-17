b99669498fc7d440da028e4189dd1e1a7d9cdced
Denys Korniienko <den.creativ.kor@gmail.com>
2021-02-17 19:45:00 +0200

    Init

---
+++ Assets/PlaneController.cs
@@ -0,0 +1,46 @@
+﻿using Obi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField] private Vector3 v;
+
+    [SerializeField]
+    private float xAngleConstraint = 15;
+
+    [SerializeField]
+    private float zAngleConstraint = 15;
+
+    [SerializeField]
+    private float angleVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var rotation = transform.eulerAngles + new Vector3(vertical, 0, horizontal) * angleVelocity;
+
+        // limit rotation
+        var xAngle = (rotation.x > 180) ? rotation.x - 360 : rotation.x;
+
+        if (xAngle > xAngleConstraint)
+            rotation.x = xAngleConstraint;
+        else if(xAngle < xAngleConstraint * -1)
+            rotation.x = xAngleConstraint * -1;
+
+        var zAngle = (rotation.z > 180) ? rotation.z - 360 : rotation.z;
+
+        if (zAngle > zAngleConstraint)
+            rotation.z = zAngleConstraint;
+        else if (zAngle < zAngleConstraint * -1)
+            rotation.z = zAngleConstraint * -1;
+
+        transform.eulerAngles = rotation;
+
+        v = rotation;
+    }
+}
---
+++ Assets/Scripts/DropletController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Obi;
+using UnityEngine;
+
+public class DropletController : MonoBehaviour
+{
+    [SerializeField]
+    private ObiSolver _obiSolver;
+
+    [SerializeField]
+    private ObiEmitter _obiEmitter;
+
+    [SerializeField] private Component _deathCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _obiSolver.OnCollision += ObiSolverOnOnCollision;
+        _obiSolver.OnParticleCollision += ObiSolverOnOnParticleCollision;
+        //_obiSolver.OnInterpolate += ObiSolverOnOnInterpolate;
+        //_obiSolver.OnSubstep += ObiSolverOnOnSubstep;
+        //_obiSolver.OnUpdateParameters += ObiSolverOnOnUpdateParameters;
+
+       // _obiEmitter.OnKillParticle += ObiEmitterOnOnKillParticle;
+    }
+
+    private void ObiSolverOnOnParticleCollision(ObiSolver solver, ObiSolver.ObiCollisionEventArgs contacts)
+    {
+       // Debug.Log("Particle Collision");
+    }
+
+    private void ObiSolverOnOnCollision(ObiSolver solver, ObiSolver.ObiCollisionEventArgs contacts)
+    {
+        var contact = contacts.contacts[0];
+
+        // this one is an actual collision:
+        if (contact.distance < 0.01)
+        {
+            ObiCollider.idToCollider.TryGetValue(contact.other, out var collider);
+            if (collider != null)
+            {
+                if (collider.name == "Plane (1)")
+                {
+                    Debug.Log("Death!");
+                }
+                // do something with the collider.
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
